d63544f592256b0f8fb02a54102f29e45dfac697
Wispy <owispyo@gmail.com>
2016-04-27 09:31:41 -0400

    put insightedge and spark libs into zeppelin classpath

--- insightedge-packager/src/main/resources/start-zeppelin.sh
+++ insightedge-packager/src/main/resources/start-zeppelin.sh
@@ -6,4 +6,8 @@ if [ -z "${INSIGHTEDGE_HOME}" ]; then
   export INSIGHTEDGE_HOME="$(cd "`dirname "$0"`"/..; pwd)"
 fi
 
+# add spark, datagrid and InsightEdge JARs to Zeppelin classpath
+. ${INSIGHTEDGE_HOME}/sbin/common-insightedge.sh
+export CLASSPATH="$(get_libs ':'):$(find ${INSIGHTEDGE_HOME}/lib -name "spark-assembly-*.jar")"
+
 . "${INSIGHTEDGE_HOME}/zeppelin/bin/zeppelin-daemon.sh" start
