30d9a35c39858fc491c9aa3d8059e98515e51246
fe2s <aka.fe2s@gmail.com>
2016-05-16 14:13:08 +0300

    [IE-67] Scala RDD API should work with Java space model

--- insightedge-core/src/test/scala/com/gigaspaces/spark/rdd/GigaSpacesRDDSpec.scala
+++ insightedge-core/src/test/scala/com/gigaspaces/spark/rdd/GigaSpacesRDDSpec.scala
@@ -1,13 +1,13 @@
 package com.gigaspaces.spark.rdd
 
 import com.gigaspaces.spark.implicits._
-import com.gigaspaces.spark.utils.{GigaSpaceFactory, GigaSpaces, GsConfig, Spark}
+import com.gigaspaces.spark.utils._
 import org.apache.spark.rdd.RDD
 import org.scalatest._
 
-class GigaSpacesRDDSpec extends FunSpec with GsConfig with GigaSpaces with Spark {
+class GigaSpacesRDDSpec extends FlatSpec with GsConfig with GigaSpaces with Spark {
 
-  it("should successfully store data to Data Grid") {
+  it should "store data to Data Grid" taggedAs ScalaSpaceClass in {
     val rdd: RDD[Data] = sc.parallelize(dataSeq(1000))
     rdd.saveToGrid()
 
@@ -19,7 +19,20 @@ class GigaSpacesRDDSpec extends FunSpec with GsConfig with GigaSpaces with Spark
     }
   }
 
-  it("should successfully read data from Data Grid") {
+  it should "store data to Data Grid [java]" taggedAs JavaSpaceClass in {
+    val rdd: RDD[JData] = sc.parallelize(jDataSeq(1000))
+    rdd.saveToGrid()
+
+    val dataCount = spaceProxy.count(new JData())
+    assert(dataCount == 1000, "Data objects weren't written to the space")
+    for (i <- 1L to 1000) {
+      val template = new JData()
+      template.setRouting(i)
+      assert(spaceProxy.read(template).getData == "data" + i, "Data objects weren't written to the space")
+    }
+  }
+
+  it should "read data from Data Grid" taggedAs ScalaSpaceClass in {
     writeDataSeqToDataGrid(1000)
     val rdd = sc.gridRdd[Data]()
     val sum = rdd.map(data => data.routing).sum()
@@ -27,7 +40,15 @@ class GigaSpacesRDDSpec extends FunSpec with GsConfig with GigaSpaces with Spark
     assert(sum == expectedSum, "")
   }
 
-  it("should read only data of RDD type") {
+  it should "read data from Data Grid [java]" taggedAs JavaSpaceClass in {
+    writeJDataSeqToDataGrid(1000)
+    val rdd = sc.gridRdd[JData]()
+    val sum = rdd.map(data => data.getRouting.toLong).sum()
+    val expectedSum = (1 to 1000).sum
+    assert(sum == expectedSum, "")
+  }
+
+  it should "read only data of RDD type" taggedAs ScalaSpaceClass in {
     writeDataSeqToDataGrid(10)
 
     val strings = (1 to 20).map(i => new GridString("string " + i))
@@ -37,14 +58,24 @@ class GigaSpacesRDDSpec extends FunSpec with GsConfig with GigaSpaces with Spark
     assert(rdd.count() == 20)
   }
 
-  it("should have bucketed partitions set by user") {
+  it should "read only data of RDD type [java]" taggedAs JavaSpaceClass in {
+    writeJDataSeqToDataGrid(10)
+
+    val strings = (1 to 20).map(i => new GridString("string " + i))
+    spaceProxy.writeMultiple(randomBucket(strings).toArray)
+
+    val rdd = sc.gridRdd[GridString]()
+    assert(rdd.count() == 20)
+  }
+
+  it should "have bucketed partitions set by user" taggedAs ScalaSpaceClass in {
     val rdd = sc.gridRdd[GridString](splitCount = Some(8))
     assert(rdd.getNumPartitions == 16)
     assert(GigaSpaceFactory.clusteredCacheSize() == 1)
     assert(GigaSpaceFactory.directCacheSize() == 2)
   }
 
-  it("should have default partitions as 4x grid partitions") {
+  it should "have default partitions as 4x grid partitions" taggedAs ScalaSpaceClass in {
     val rdd = sc.gridRdd[GridString](None)
     assert(rdd.getNumPartitions == 4 * 2)
     assert(GigaSpaceFactory.clusteredCacheSize() == 1)
