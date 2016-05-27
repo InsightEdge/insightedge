b74f69ba7eb2948d11c5368b633f4577676cda95
fe2s <aka.fe2s@gmail.com>
2016-05-27 10:05:11 -0400

    checpoint

--- insightedge-integration-tests/src/test/scala/com/gigaspaces/spark/ZeppelinTutorialSpec.scala
+++ insightedge-integration-tests/src/test/scala/com/gigaspaces/spark/ZeppelinTutorialSpec.scala
@@ -12,11 +12,22 @@ import play.api.libs.json.JsString
   */
 class ZeppelinTutorialSpec extends FlatSpec with InsightEdgeDocker {
 
+  val TutorialId = "INSIGHTEDGE-BASIC"
+
   "Zeppelin" should "have InsightEdge Basics tutorial" in {
     val resp = wsClient.url(s"$zeppelinUrl/api/notebook").get()
     val notebookIds = jsonBody(resp) \\ "id"
 
-    assert(notebookIds.contains(JsString("INSIGHTEDGE-BASIC")))
+    assert(notebookIds.contains(JsString(TutorialId)))
   }
+
+  it should "be possible to run InsightEdge Basics tutorial" in {
+//    val resp = wsClient.url(s"$zeppelinUrl/api/notebook").get()
+//    val notebookIds = jsonBody(resp) \\ "id"
+//
+//    assert(notebookIds.contains(JsString("INSIGHTEDGE-BASIC")))
+  }
+
+
 
 }
