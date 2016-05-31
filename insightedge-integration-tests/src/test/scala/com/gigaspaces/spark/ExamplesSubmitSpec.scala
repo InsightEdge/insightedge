4f2229b242ffb85cf893ce8986ebc5809905ea4a
fe2s <aka.fe2s@gmail.com>
2016-05-31 09:04:19 -0400

    [IE-78]  Integration test that submits examples.jar to Spark

---
+++ insightedge-integration-tests/src/test/scala/com/gigaspaces/spark/ExamplesSubmitSpec.scala
@@ -0,0 +1,58 @@
+package com.gigaspaces.spark
+
+import com.gigaspaces.spark.fixture.InsightEdgeDocker
+import com.gigaspaces.spark.utils.DockerUtils
+import com.gigaspaces.spark.utils.DockerUtils.dockerExec
+import org.scalatest.FlatSpec
+
+/**
+  * Verifies that we can submit InsightEdge examples
+  *
+  * @author Oleksiy_Dyagilev
+  */
+class ExamplesSubmitSpec extends FlatSpec with InsightEdgeDocker {
+
+  "insightedge-submit.sh " should "submit examples from insightedge-examples.jar" in {
+    val containerId = runningContainerId()
+    val exampleClassNames = Seq(
+      "basic.SaveRdd",
+      "basic.LoadRdd",
+      "basic.LoadRddWithSql",
+      "basic.PersistDataFrame",
+      "basic.PersistDataFrame",
+      "mllib.SaveAndLoadMLModel",
+      "offheap.OffHeapPersistence"
+    )
+
+    exampleClassNames.foreach { className =>
+      val fullClassName = s"com.gigaspaces.insightedge.examples.$className"
+      val command =
+        s"""/opt/gigaspaces-insightedge/bin/insightedge-submit
+          |--class $fullClassName
+          |--master spark://127.0.0.1:7077
+          |/opt/gigaspaces-insightedge/quickstart/insightedge-examples.jar""".stripMargin
+
+      val exitCode = dockerExec(containerId, command)
+      assert(exitCode == 0)
+    }
+  }
+
+  "insightedge-submit.sh " should "fail with wrong space name" in {
+    val containerId = runningContainerId()
+    val spaceName = "non-existing-space"
+    val command =
+      s"""/opt/gigaspaces-insightedge/bin/insightedge-submit
+          |--class com.gigaspaces.insightedge.examples.basic.SaveRdd
+          |--master spark://127.0.0.1:7077
+          |/opt/gigaspaces-insightedge/quickstart/insightedge-examples.jar
+          |spark://127.0.0.1:7077
+          |$spaceName
+          |insightedge
+          |127.0.0.1:4174""".stripMargin
+
+    val exitCode = dockerExec(containerId, command)
+    assert(exitCode != 0)
+  }
+
+
+}
