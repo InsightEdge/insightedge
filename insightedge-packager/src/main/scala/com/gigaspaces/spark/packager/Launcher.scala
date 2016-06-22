e8fa1681126864dd7d9fb0b024c6a15b23944360
fe2s <aka.fe2s@gmail.com>
2016-06-22 04:55:23 -0400

    don't unpack advanced_scripts.zip

--- insightedge-packager/src/main/scala/com/gigaspaces/spark/packager/Launcher.scala
+++ insightedge-packager/src/main/scala/com/gigaspaces/spark/packager/Launcher.scala
@@ -11,6 +11,7 @@ import org.apache.commons.io.filefilter.TrueFileFilter
   * @author Leonid_Poliakov
   */
 object Launcher {
+
   def main(args: Array[String]) {
     val project = parameter("Project folder" -> "project.directory")
     val version = parameter("Project version" -> "project.version")
@@ -65,7 +66,6 @@ object Launcher {
 
     run("Unpacking Gigaspaces datagrid") {
       unzip(grid, s"$output/datagrid", cutRootFolder = true)
-      unzip(s"$output/datagrid/bin/advanced_scripts.zip", s"$output/datagrid/bin/", cutRootFolder = false)
     }
     run("Adding Gigaspaces datagrid license key") {
       copy(s"$resources/gslicense.xml", s"$output/datagrid/gslicense.xml")
