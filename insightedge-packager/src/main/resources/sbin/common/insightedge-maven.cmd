2e4e8b704f6974e3fb51723bdc04a0ec4e2d458f
Wispy <owispyo@gmail.com>
2016-07-22 05:28:01 -0400

    [IE-149] [maven] sbin\insightedge-maven.cmd pollutes Windows env with INSIGHTEDGE_HOME
    Closes #60

--- insightedge-packager/src/main/resources/sbin/common/insightedge-maven.cmd
+++ insightedge-packager/src/main/resources/sbin/common/insightedge-maven.cmd
@@ -1,32 +1,6 @@
 @echo off
 
-set INSIGHTEDGE_VER=1.0.0
+rem This is the entry point for installing InsightEdge artifacts. To avoid polluting the
+rem environment, it just launches a new cmd to do the real work.
 
-if "x%INSIGHTEDGE_HOME%"=="x" (
-    set INSIGHTEDGE_HOME=%~dp0..
-)
-
-echo Installing InsightEdge %INSIGHTEDGE_VER% artifacts
-
-call mvn install:install-file ^
-    -Dpackaging=pom ^
-    -Dfile=%INSIGHTEDGE_HOME%\tools\maven\poms\insightedge-package\pom.xml ^
-    -DpomFile=%INSIGHTEDGE_HOME%\tools\maven\poms\insightedge-package\pom.xml
-
-call mvn install:install-file ^
- -DgroupId=com.gigaspaces.insightedge ^
- -DcreateChecksum=true ^
- -DartifactId=insightedge-core ^
- -Dversion=%INSIGHTEDGE_VER% ^
- -DpomFile=%INSIGHTEDGE_HOME%\tools\maven\poms\insightedge-core\pom.xml ^
- -Dpackaging=jar ^
- -Dfile=%INSIGHTEDGE_HOME%\lib\insightedge-core-%INSIGHTEDGE_VER%.jar
-
-call mvn install:install-file ^
- -DgroupId=com.gigaspaces.insightedge ^
- -DcreateChecksum=true ^
- -DartifactId=gigaspaces-scala ^
- -Dversion=%INSIGHTEDGE_VER% ^
- -DpomFile=%INSIGHTEDGE_HOME%\tools\maven\poms\gigaspaces-scala\pom.xml ^
- -Dpackaging=jar ^
- -Dfile=%INSIGHTEDGE_HOME%\lib\gigaspaces-scala-%INSIGHTEDGE_VER%.jar
+cmd /V /E /C %~dp0insightedge-maven2.cmd %*
