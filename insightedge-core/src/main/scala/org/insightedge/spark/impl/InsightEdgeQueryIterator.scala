47d3668b35d1b1cdb96337ad2b66889d77ee0003
fe2s <aka.fe2s@gmail.com>
2016-08-23 12:43:22 +0300

    [IE-160] [core] use SpaceIterator instead of GSIterator
    Closes #69

--- insightedge-core/src/main/scala/org/insightedge/spark/impl/InsightEdgeQueryIterator.scala
+++ insightedge-core/src/main/scala/org/insightedge/spark/impl/InsightEdgeQueryIterator.scala
@@ -1,11 +1,11 @@
 package org.insightedge.spark.impl
 
-import com.j_spaces.core.client.GSIterator
+import com.gigaspaces.client.iterator.SpaceIterator
 
-private[spark] class InsightEdgeQueryIterator[T](cur: GSIterator) extends Iterator[T] {
+private[spark] class InsightEdgeQueryIterator[T](cur: SpaceIterator[T]) extends Iterator[T] {
 
   override def hasNext: Boolean = cur.hasNext
 
-  override def next(): T = cur.next().asInstanceOf[T]
+  override def next(): T = cur.next()
 
 }
