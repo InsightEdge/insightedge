6651e45299656b206b5b96f3e23f53e903ff25c1
fe2s <aka.fe2s@gmail.com>
2016-09-01 15:25:27 +0300

    fixing geospatial .. still one test if failing

---
+++ insightedge-core/src/main/scala/org/apache/spark/sql/insightedge/udt/GeoUDTRegistration.scala
@@ -0,0 +1,28 @@
+package org.apache.spark.sql.insightedge.udt
+
+import org.apache.spark.sql.types.UDTRegistration
+
+/**
+  * Registers Geo Shape UDTs
+  *
+  * @author Oleksiy_Dyagilev
+  */
+object GeoUDTRegistration {
+
+  /**
+    * register Geo Shape UDTs if not already registered
+    */
+  def registerIfNotAlready(): Unit ={
+    registerIfNotAlready("org.openspaces.spatial.shapes.Circle", "org.apache.spark.sql.insightedge.udt.CircleUDT")
+    registerIfNotAlready("org.openspaces.spatial.shapes.Point", "org.apache.spark.sql.insightedge.udt.PointUDT")
+    registerIfNotAlready("org.openspaces.spatial.shapes.Polygon", "org.apache.spark.sql.insightedge.udt.PolygonUDT")
+    registerIfNotAlready("org.openspaces.spatial.shapes.Rectangle", "org.apache.spark.sql.insightedge.udt.PolygonUDT")
+  }
+
+  private def registerIfNotAlready(userClass: String, udtClass: String) = {
+    if (!UDTRegistration.exists(userClass)){
+      UDTRegistration.register(userClass, udtClass)
+    }
+  }
+
+}
