b327705e7cc8bb483e571832134e2508bd8a589b
Danylo Hurin <danylo@gigaspaces.com>
2016-09-05 17:24:40 +0300

    [IE-188] [ci] Maven integration test uses wrong branch
    Closes #77

--- insightedge-integration-tests/src/test/resources/docker/maven-install-libs/run.sh
+++ insightedge-integration-tests/src/test/resources/docker/maven-install-libs/run.sh
@@ -3,16 +3,18 @@
 # Starts a docker image with pre-installed Maven/Sbt and runs tests.
 #
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Illegal number of parameters."
-    echo "Usage: run.sh <dir with InsightEdge zip> <distribution version>"
+    echo "Usage: run.sh <dir with InsightEdge zip> <distribution version> <Git branch>"
     exit 1
 fi
 
 LOCAL_DOWNLOAD_DIR=$1
 VERSION=$2
+BRANCH=$3
 echo "Local download dir: $LOCAL_DOWNLOAD_DIR"
 echo "Version: $VERSION"
+echo "Git branch: $BRANCH"
 
 # Stop if anything is running
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -20,6 +22,6 @@ $DIR/stop.sh
 
 docker run --name maven-install-libs-test-image -P -d -v $LOCAL_DOWNLOAD_DIR:/download insightedge-tests-maven-install-libs:$VERSION
 
-docker exec --user ie-user maven-install-libs-test-image /home/ie-user/maven-install-libs-test.sh
+docker exec --user ie-user maven-install-libs-test-image /home/ie-user/maven-install-libs-test.sh $BRANCH
 
 
