fb7ded6e5142453ce21329f54f88adb93dfe4ca7
kobi <kobikis@gmail.com>
2016-12-04 15:39:45 +0200

    [IE-199] [core] commited by mistake

--- insightedge-integration-tests/tests/src/test/scala/org/insightedge/spark/utils/BuildUtils.scala
+++ insightedge-integration-tests/tests/src/test/scala/org/insightedge/spark/utils/BuildUtils.scala
@@ -4,9 +4,8 @@ package org.insightedge.spark.utils
   * @author Oleksiy_Dyagilev
   */
 object BuildUtils {
-
-  val BuildVersion = Option(System.getProperty("dist.version", "1.1.0-SNAPSHOT")).getOrElse(throw new Exception("System property 'dist.version' is not set"))
-  val BuildEdition = Option(System.getProperty("dist.edition", "premium")).getOrElse(throw new Exception("System property 'dist.edition' is not set"))
-  val GitBranch = Option(System.getProperty("git.branch", "ffffff")).getOrElse(throw new Exception("System property 'git.branch' is not set"))
-  val IEHome = Option(System.getProperty("dist.dir", "/home/kobi/opt/insightedge/1.0/gigaspaces-insightedge-1.1.0-SNAPSHOT-premium")).getOrElse(throw new Exception("System property 'dist.dir' is not set"))
-}
+  val BuildVersion = Option(System.getProperty("dist.version")).getOrElse(throw new Exception("System property 'dist.version' is not set"))
+  val BuildEdition = Option(System.getProperty("dist.edition")).getOrElse(throw new Exception("System property 'dist.edition' is not set"))
+  val GitBranch = Option(System.getProperty("git.branch")).getOrElse(throw new Exception("System property 'git.branch' is not set"))
+  val IEHome = Option(System.getProperty("dist.dir")).getOrElse(throw new Exception("System property 'dist.dir' is not set"))
+} 
