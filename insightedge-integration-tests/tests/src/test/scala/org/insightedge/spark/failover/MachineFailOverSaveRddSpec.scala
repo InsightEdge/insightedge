e66a9af3509076ac224414bd2efcca19f9b6e1c5
Tamir Schwarz <tamirs2005@gmail.com>
2017-06-14 11:08:53 +0300

    IE-205 - change Jobs jar to be without version and change paths accordingly

--- insightedge-integration-tests/tests/src/test/scala/org/insightedge/spark/failover/MachineFailOverSaveRddSpec.scala
+++ insightedge-integration-tests/tests/src/test/scala/org/insightedge/spark/failover/MachineFailOverSaveRddSpec.scala
@@ -42,7 +42,7 @@ import org.scalatest.{BeforeAndAfterAll, FlatSpec, Suite}
 class MachineFailOverSaveRddSpec extends FlatSpec with BeforeAndAfterAll {
   self: Suite =>
 
-  private val JOBS = s"/opt/insightedge/quickstart/scala/jobs-${BuildUtils.BuildVersion}.jar"
+  private val JOBS = s"/opt/insightedge/quickstart/scala/jobs.jar"
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
