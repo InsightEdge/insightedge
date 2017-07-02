bbefcf889e6d1f773cf401321905d3a69cb0879c
Yohana Khoury <yohana@yohanakhoury.com>
2017-07-02 20:45:33 +0300

    IE-205 Return and use publish-artifacts + fix build name

--- build/ie-release.sh
+++ build/ie-release.sh
@@ -314,86 +314,8 @@ function announce_step {
 # upload zip to s3.
 
 function publish_ie {
-
-    baseOutputFolder="${WORKSPACE}/insightedge/insightedge-packager/target/"
-    premiumZipFileName="gigaspaces-insightedge-${FINAL_IE_BUILD_VERSION}-premium.zip"
-    communityZipFileName="gigaspaces-insightedge-${FINAL_IE_BUILD_VERSION}-community.zip"
-
-    host=${storageHost}
-    echo "host=${host}"
-
-    port=${storagePort}
-    echo "port=${port}"
-
-    user=${storageUser}
-    echo "user=${user}"
-
-    branch=${BRANCH}
-    echo "branch=${BRANCH}"
-
-    version=${IE_VERSION}
-    echo "version=${IE_VERSION}"
-
-    buildNumber=${IE_FINAL_BUILD_NUMBER}
-    echo "buildNumber=${buildNumber}"
-
-    base_path="/home/giga/backup-store/insightedge"
-    build_path="${version}/${branch}/${buildNumber}"
-    path_on_server="${base_path}/${build_path}"
-
-    communityZipFilePath=${baseOutputFolder}/community/${communityZipFileName}/
-    premiumZipFilePath=${baseOutputFolder}/premium/${premiumZipFileName}
-
-    echo "check if insightedge community zip exists"
-    ls -l ${communityZipFilePath}
-
-    echo "check if insightedge premium zip exists"
-    ls -l ${premiumZipFilePath}
-
-    echo "creating dir in server - [$path_on_server]"
-    ssh ${user}@${host} mkdir -p ${path_on_server}
-    ssh ${user}@${host} chown -R "${user}:domain\ users" ${base_path}/${version}
-    ssh ${user}@${host} chmod -R 755 ${base_path}/${version}
-
-    echo "coping resources to: [$host] to folder in server:[${path_on_server}]"
-    scp ${communityZipFilePath} ${premiumZipFilePath} ${baseOutputFolder}/*.json ${baseOutputFolder}/integration-tests-sources.zip ${baseOutputFolder}/metadata.txt ${baseOutputFolder}/newman-artifacts.zip ${user}@${host}:${path_on_server}
-    ssh ${user}@${host} chmod -R 755 ${path_on_server}
-
-    echo "starting newman submitter process..."
-    java -version
-    wget http://xap-test.s3.amazonaws.com/qa/newman/newman-submitter-1.0.jar -O ${WORKSPACE}/newman-submitter-1.0.jar
-    sumbitter_jar=${WORKSPACE}/newman-submitter-1.0.jar
-    if [ -f $sumbitter_jar ]; then
-        BASE_WEB_URI=http://${host}:${port}/insightedge
-        WEB_PATH_TO_BUILD=${BASE_WEB_URI}/${build_path}
-        echo "web url for build resources: ${WEB_PATH_TO_BUILD}"
-
-        export NEWMAN_HOST=${newmanHost="xap-newman"}
-        export NEWMAN_PORT=${newmanPort="8443"}
-        export NEWMAN_USER_NAME=${newmanUsername="root"}
-        export NEWMAN_PASSWORD=${newmanPassword="root"}
-        export NEWMAN_BUILD_BRANCH=${branch}
-        export NEWMAN_BUILD_NUMBER=${buildNumber}
-        export NEWMAN_BUILD_TAGS=${NEWMAN_TAGS}
-        export NEWMAN_BUILD_TESTS_METADATA=${WEB_PATH_TO_BUILD}/ie-integration-tests.json
-        export NEWMAN_BUILD_SHAS_FILE=${WEB_PATH_TO_BUILD}/metadata.txt
-        export NEWMAN_BUILD_RESOURCES=${WEB_PATH_TO_BUILD}/integration-tests-sources.zip,${WEB_PATH_TO_BUILD}/${communityZipFileName},${WEB_PATH_TO_BUILD}/${premiumZipFileName},${WEB_PATH_TO_BUILD}/newman-artifacts.zip
-
-        echo "NEWMAN_BUILD_BRANCH=${NEWMAN_BUILD_BRANCH}"
-        echo "NEWMAN_BUILD_NUMBER=${NEWMAN_BUILD_NUMBER}"
-        echo "NEWMAN_HOST=${NEWMAN_HOST}"
-        echo "NEWMAN_USER_NAME=${NEWMAN_USER_NAME}"
-        echo "NEWMAN_BUILD_TESTS_METADATA=${NEWMAN_BUILD_TESTS_METADATA}"
-        echo "NEWMAN_BUILD_SHAS_FILE=${NEWMAN_BUILD_SHAS_FILE}"
-        echo "NEWMAN_BUILD_RESOURCES=${NEWMAN_BUILD_RESOURCES}"
-        java -cp ${sumbitter_jar} com.gigaspaces.newman.NewmanBuildSubmitter
-        echo "finished newman submitter process successfully"
-    else
-        echo "skipping newman submitter process because the file ${sumbitter_jar} does not exists"
-        exit 1
-    fi
-
-
+    local cmd="package -pl insightedge-packager -P publish-artifacts  -DskipTests=true -Dinsightedge.version=${IE_VERSION} -Dinsightedge.branch=$BRANCH -Dinsightedge.build.number=${IE_FINAL_BUILD_NUMBER} -Dinsightedge.milestone=${MILESTONE} -Dnewman.tags=$NEWMAN_TAGS -Dmaven.repo.local=$M2/repository"
+    execute_command "Publish IE to Newman" "$1" "$cmd"
 }
 
 
@@ -507,7 +429,7 @@ function release_ie {
     delete_temp_branch "$ie_zeppelin_folder" "$temp_branch_name"
 
     announce_step "publish ie to hercules and newman"
-    publish_ie
+    publish_ie "$ie_folder"
 
     announce_step "uploading ie-community zip"
     upload_ie_zip "$ie_folder" "ie-community"
