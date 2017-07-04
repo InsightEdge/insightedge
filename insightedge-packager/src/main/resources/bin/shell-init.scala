477305e30f1a9bc43e782756834945e81f352273
Tamir Schwarz <tamirs2005@gmail.com>
2017-07-04 18:01:33 +0300

    Fix script - try 1

--- insightedge-packager/src/main/resources/bin/shell-init.scala
+++ insightedge-packager/src/main/resources/bin/shell-init.scala
@@ -1,11 +1,14 @@
-import org.insightedge.spark.implicits.all._
+import org.apache.spark.{SparkConf, SparkContext}
 import org.insightedge.spark.context.InsightEdgeConfig
-import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.sql.SQLContext
+import org.insightedge.spark.implicits.all._
+import org.apache.spark.sql.SparkSession
 
 System.setProperty("net.jini.discovery.LookupLocatorDiscovery.level", "OFF")
-val ieConfig = InsightEdgeConfig("insightedge-space", Some("insightedge"), Some("127.0.0.1:4174"))
+val settings = Array("spark://127.0.0.1:7077", "insightedge-space", "insightedge", "127.0.0.1:4174")
+val Array(master, space, groups, locators) = settings
+val ieConfig = InsightEdgeConfig(space, Some(groups), Some(locators))
+spark.stop()
+
+val spark = SparkSession.builder.master(master).appName("my init shell").insightEdgeConfig(ieConfig).getOrCreate()
 val conf = new SparkConf().setAll(sc.getConf.getAll).setInsightEdgeConfig(ieConfig)
-sc.stop()
-val sc = new SparkContext(conf)
-val sqlContext = new SQLContext(sc)
+val sc = spark.sparkContext
