d9ce5195dcbbaced3098720b00c4b5caea71531c
Tamir Schwarz <tamirs2005@gmail.com>
2017-07-04 18:01:33 +0300

    IE-214 fix ie spark and python shell

--- insightedge-packager/src/main/resources/bin/shell-init.py
+++ insightedge-packager/src/main/resources/bin/shell-init.py
@@ -16,8 +16,6 @@
 #
 
 """
-This is an enhanced version of python/pyspark/shell.py
-
 An interactive shell.
 
 This file is designed to be launched as a PYTHONSTARTUP script.
@@ -26,42 +24,52 @@ This file is designed to be launched as a PYTHONSTARTUP script.
 import atexit
 import os
 import platform
+import sys
 
 import py4j
 
 import pyspark
 from pyspark.context import SparkContext
-from pyspark.conf import SparkConf
-from pyspark.sql import SQLContext, HiveContext
+from pyspark.sql import SparkSession, SQLContext
 from pyspark.storagelevel import StorageLevel
 
-# this is the deprecated equivalent of ADD_JARS
-add_files = None
-if os.environ.get("ADD_FILES") is not None:
-    add_files = os.environ.get("ADD_FILES").split(',')
+    # InsightEdge config
+if len(sys.argv) < 4:
+    spaceName = "insightedge-space"
+    lookupGroup = "insightedge"
+    lookupLocator = "127.0.0.1:4174"
+else:
+    spaceName = sys.argv[1]
+    lookupGroup = sys.argv[2]
+    lookupLocator = sys.argv[3]
+
+print("InsightEdge config: %s %s %s" % (spaceName, lookupGroup, lookupLocator))
 
 if os.environ.get("SPARK_EXECUTOR_URI"):
     SparkContext.setSystemProperty("spark.executor.uri", os.environ["SPARK_EXECUTOR_URI"])
 
-# InsightEdge configuration
-conf = SparkConf()
-conf.set("spark.insightedge.space.name", "insightedge-space")
-conf.set("spark.insightedge.space.lookup.group", "insightedge")
-conf.set("spark.insightedge.space.lookup.locator", "127.0.0.1:4174")
-
-sc = SparkContext(pyFiles=add_files, conf=conf)
-atexit.register(lambda: sc.stop())
+SparkContext._ensure_initialized()
 
 try:
     # Try to access HiveConf, it will raise exception if Hive is not added
-    sc._jvm.org.apache.hadoop.hive.conf.HiveConf()
-    sqlContext = HiveContext(sc)
+    SparkContext._jvm.org.apache.hadoop.hive.conf.HiveConf()
+    spark = SparkSession.builder\
+        .enableHiveSupport()\
+        .config("spark.insightedge.space.name", spaceName) \
+        .config("spark.insightedge.space.lookup.group", lookupGroup) \
+        .config("spark.insightedge.space.lookup.locator", lookupLocator) \
+        .getOrCreate()
 except py4j.protocol.Py4JError:
-    sqlContext = SQLContext(sc)
+    spark = SparkSession.builder.config("spark.insightedge.space.name", spaceName).config("spark.insightedge.space.lookup.group", lookupGroup).config("spark.insightedge.space.lookup.locator", lookupLocator).getOrCreate()
 except TypeError:
-    sqlContext = SQLContext(sc)
+    spark = SparkSession.builder.config("spark.insightedge.space.name", spaceName).config("spark.insightedge.space.lookup.group", lookupGroup).config("spark.insightedge.space.lookup.locator", lookupLocator).getOrCreate()
+
+sc = spark.sparkContext
+sql = spark.sql
+atexit.register(lambda: sc.stop())
 
 # for compatibility
+sqlContext = spark._wrapped
 sqlCtx = sqlContext
 
 print("""Welcome to
@@ -75,14 +83,12 @@ print("Using Python version %s (%s, %s)" % (
     platform.python_version(),
     platform.python_build()[0],
     platform.python_build()[1]))
-print("SparkContext available as sc, %s available as sqlContext." % sqlContext.__class__.__name__)
-
-if add_files is not None:
-    print("Warning: ADD_FILES environment variable is deprecated, use --py-files argument instead")
-    print("Adding files: [%s]" % ", ".join(add_files))
+print("SparkSession available as 'spark'.")
 
 # The ./bin/pyspark script stores the old PYTHONSTARTUP value in OLD_PYTHONSTARTUP,
 # which allows us to execute the user's PYTHONSTARTUP file:
 _pythonstartup = os.environ.get('OLD_PYTHONSTARTUP')
 if _pythonstartup and os.path.isfile(_pythonstartup):
-    execfile(_pythonstartup)
+    with open(_pythonstartup) as f:
+        code = compile(f.read(), _pythonstartup, 'exec')
+        exec(code)
--- insightedge-packager/src/main/resources/bin/shell-init.scala
+++ insightedge-packager/src/main/resources/bin/shell-init.scala
@@ -1,11 +1,15 @@
-import org.insightedge.spark.implicits.all._
+import org.apache.spark.{SparkConf, SparkContext}
 import org.insightedge.spark.context.InsightEdgeConfig
-import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.sql.SQLContext
+import org.insightedge.spark.implicits.all._
+import org.apache.spark.sql.SparkSession
+import org.insightedge.spark.implicits.all._
 
 System.setProperty("net.jini.discovery.LookupLocatorDiscovery.level", "OFF")
-val ieConfig = InsightEdgeConfig("insightedge-space", Some("insightedge"), Some("127.0.0.1:4174"))
-val conf = new SparkConf().setAll(sc.getConf.getAll).setInsightEdgeConfig(ieConfig)
-sc.stop()
-val sc = new SparkContext(conf)
-val sqlContext = new SQLContext(sc)
+val settings = Array("local[*]", "insightedge-space", "insightedge", "127.0.0.1:4174")
+val Array(master, space, groups, locators) = settings
+val ieConfig = InsightEdgeConfig(space, Some(groups), Some(locators))
+spark.stopInsightEdgeContext()
+
+val spark = SparkSession.builder.master(master).appName("my init shell").insightEdgeConfig(ieConfig).getOrCreate()
+val sc = spark.sparkContext
+import spark.implicits._
