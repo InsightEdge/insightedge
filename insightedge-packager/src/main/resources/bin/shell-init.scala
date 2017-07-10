7908c4369322e8cb0cea352094de1cf0c04f0e1b
Yohana Khoury <yohana@yohanakhoury.com>
2017-07-10 17:18:13 +0300

    IE-214 Use spark master url from previous sparksession

--- insightedge-packager/src/main/resources/bin/shell-init.scala
+++ insightedge-packager/src/main/resources/bin/shell-init.scala
@@ -5,11 +5,11 @@ import org.apache.spark.sql.SparkSession
 import org.insightedge.spark.implicits.all._
 
 System.setProperty("net.jini.discovery.LookupLocatorDiscovery.level", "OFF")
-val settings = Array("local[*]", "insightedge-space", "insightedge", "127.0.0.1:4174")
-val Array(master, space, groups, locators) = settings
+val settings = Array("insightedge-space", "insightedge", "127.0.0.1:4174")
+val Array(space, groups, locators) = settings
 val ieConfig = InsightEdgeConfig(space, Some(groups), Some(locators))
+val sparkMaster = spark.conf.get("spark.master", "local[*]")
 spark.stopInsightEdgeContext()
-
-val spark = SparkSession.builder.master(master).appName("my init shell").insightEdgeConfig(ieConfig).getOrCreate()
+val spark = SparkSession.builder.master(sparkMaster).appName("my init shell").insightEdgeConfig(ieConfig).getOrCreate()
 val sc = spark.sparkContext
 import spark.implicits._
