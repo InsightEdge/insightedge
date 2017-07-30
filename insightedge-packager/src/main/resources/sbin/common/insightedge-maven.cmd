5e8a75a879ed34b609fd6523b8bd0b9ee4a80367
talmtalmelamud <talm@gigaspaces.com>
2017-07-30 13:14:04 +0300

    remove common and commuinty folders

--- insightedge-packager/src/main/resources/sbin/common/insightedge-maven.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem This is the entry point for installing InsightEdge artifacts. To avoid polluting the
-rem environment, it just launches a new cmd to do the real work.
-
-cmd /V /E /C %~dp0insightedge-maven2.cmd %*
--- insightedge-packager/src/main/resources/sbin/common/win-daemon.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem This is the entry point for running Windows daemon. To avoid polluting the
-rem environment, it just launches a new cmd to do the real work.
-
-cmd /V /E /C %~dp0win-daemon2.cmd %*
