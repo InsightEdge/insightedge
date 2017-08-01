d2bd4ca7e7f7ba98b85ec1b5afa7489b17fb0676
Yohana Khoury <yohana@yohanakhoury.com>
2017-08-01 16:21:56 +0300

    IE-222 cleanup in insightedge.sh

--- insightedge-packager/src/main/resources/sbin/premium/insightedge.sh
+++ insightedge-packager/src/main/resources/sbin/premium/insightedge.sh
@@ -60,13 +60,6 @@ display_usage() {
     echo "                 |       demo:          locally starts datagrid master, datagrid slave and zeppelin, deploys empty space"
     echo "                 |       shutdown:      stops 'master', 'slave' and 'zeppelin'"
     echo " -m, --master    |  * cluster master IP or hostname"
-    echo " -l, --locator   |    lookup locators for the grid components                    | default master:4174"
-    echo " -g, --group     |    lookup groups for the grid components                      | default insightedge"
-    echo "                 |               usage: if you have several clusters in one LAN,"
-    echo "                 |                      it's recommended to have unique group per cluster"
-    echo " -s, --size      |    grid container/manager heap size                           | default 1G"
-    echo "                 |             format:  java-style heap size string"
-    echo "                 |             example: '1G', '4096M'"
     echo " -c, --container |    (slave modes) number of grid containers to start           | default 2"
     echo " -n, --name      |    (deploy/undeploy modes) name of the deployed space         | default insightedge-space"
     echo " -t, --topology  |    (deploy mode) number of space primary and backup instances | default 2,0"
@@ -80,24 +73,24 @@ display_usage() {
     echo "     environment |  restarts grid manager with 1G heap size"
     echo "                 |  restarts grid lookup service at 127.0.0.1:4174 with group 'insightedge'"
     echo ""
-    echo " $script --mode master --path \$XAP_HOME --master 127.0.0.1"
+    echo " $script --mode master"
     echo ""
     echo "   Restart slave |  restarts spark slave that points to master at spark://127.0.0.1:7077"
     echo "        on local |  restarts 2 grid containers with 1G heap size"
     echo "     environment |"
     echo ""
-    echo " $script --mode slave --path \$XAP_HOME --master 127.0.0.1"
+    echo " $script --mode slave --master 127.0.0.1"
     echo ""
     echo "    Deploy empty |  deploys insightedge-space with 2 primary instances"
     echo "           space |  deploys insightedge-space with 2 primary instances"
     echo "                 |  cluster is searched with 127.0.0.1:4174 locator and 'insightedge' group"
     echo ""
-    echo " $script --mode deploy --path \$XAP_HOME --master 127.0.0.1"
+    echo " $script --mode deploy --master 127.0.0.1"
     echo ""
     echo "  Undeploy space |  undeploys insightedge-space"
     echo "                 |  cluster is searched with 127.0.0.1:4174 locator and 'insightedge' group"
     echo ""
-    echo " $script --mode undeploy --path \$XAP_HOME --master 127.0.0.1"
+    echo " $script --mode undeploy"
     echo ""
     exit 1
 }
@@ -105,12 +98,7 @@ display_usage() {
 define_defaults() {
     # '[]' means 'empty'
     MODE=$EMPTY
-    IE_PATH=$EMPTY
-    IE_INSTALL="false"
     CLUSTER_MASTER=$EMPTY
-    GRID_LOCATOR=$EMPTY
-    GRID_GROUP="insightedge"
-    GSC_SIZE="1G"
     GSC_COUNT="2"
     SPACE_NAME="insightedge-space"
     SPACE_TOPOLOGY="2,0"
@@ -123,25 +111,9 @@ parse_options() {
           shift
           MODE=$1
           ;;
-        "-p" | "--path")
-          shift
-          IE_PATH=$1
-          ;;
         "-m" | "--master")
           shift
           CLUSTER_MASTER=$1
-          ;;
-        "-l" | "--locator")
-          shift
-          GRID_LOCATOR=$1
-          ;;
-        "-g" | "--group")
-          shift
-          GRID_GROUP=$1
-          ;;
-        "-s" | "--size")
-          shift
-          GSC_SIZE=$1
           ;;
         "-c" | "--container")
           shift
@@ -187,12 +159,6 @@ check_options() {
 redefine_defaults() {
     if [ "$CLUSTER_MASTER" = "$EMPTY" ]; then
         CLUSTER_MASTER="127.0.0.1:7077"
-    fi
-    if [ "$GRID_LOCATOR" = "$EMPTY" ]; then
-        GRID_LOCATOR="$CLUSTER_MASTER:4174"
-    fi
-    if [ "$IE_PATH" = "$EMPTY" ]; then
-        IE_PATH="$XAP_HOME"
     fi
 }
 
