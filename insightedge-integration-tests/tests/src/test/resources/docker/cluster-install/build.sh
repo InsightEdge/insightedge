cf9f624a8a04b6e516eace627d094177efd40895
Yohana Khoury <yohana@yohanakhoury.com>
2017-08-06 13:34:31 +0300

    IE-222 removed ClusterInstallTest and its resources (Dockerfile, scripts, etc)

--- insightedge-integration-tests/tests/src/test/resources/docker/cluster-install/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# Builds docker image. Used for dev purpose.
-# Auto-test builds the image with maven plugin.
-#
-
-
-docker build -t insightedge-tests-cluster-install .
--- insightedge-integration-tests/tests/src/test/resources/docker/cluster-install/exec_master.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# Helper script for debug purpose. Not used by auto-tests
-#
-
-docker exec --user ie-user -it master /bin/bash
