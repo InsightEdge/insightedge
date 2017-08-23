a12065d520a9bf3c8f36e4b95bb9f0d1e8186463
Tamir Schwarz <tamirs2005@gmail.com>
2017-08-23 09:27:38 +0300

    PACKAGING - 1.1
    (cherry picked from commit d36eee2)

--- insightedge-packager/src/main/resources/bin/insightedge-submit2.cmd
+++ insightedge-packager/src/main/resources/bin/insightedge-submit2.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-call %~dp0..\..\conf\insightedge-env.cmd
+call %~dp0..\conf\insightedge-env.cmd
 
-call %SPARK_HOME%\bin\spark-submit --driver-class-path=%INSIGHTEDGE_CORE_CP% "$@"
+call %SPARK_HOME%\bin\spark-submit --driver-class-path=%INSIGHTEDGE_CORE_CP%  %*
