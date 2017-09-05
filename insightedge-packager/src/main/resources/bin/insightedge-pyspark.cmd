51c96f99c556f80f08f871028f5f49d5aad45eb2
nivin <niv@gigaspaces.com>
2017-09-05 15:16:17 +0300

    Removed redundant insightedge-pyspark2.cmd

--- insightedge-packager/src/main/resources/bin/insightedge-pyspark.cmd
+++ insightedge-packager/src/main/resources/bin/insightedge-pyspark.cmd
@@ -1,6 +1,27 @@
 @echo off
 
-rem This is the entry point for running PySpark. To avoid polluting the
-rem environment, it just launches a new cmd to do the real work.
+call %~dp0..\conf\insightedge-env.cmd
 
-cmd /V /E /C %~dp0insightedge-pyspark2.cmd %*
+rem Figure out where the Spark framework is installed
+
+if "x%SPARK_HOME%"=="x" (
+  set SPARK_HOME="%XAP_HOME%\insightedge\spark"
+)
+
+call %SPARK_HOME%\bin\load-spark-env.cmd
+set _SPARK_CMD_USAGE=Usage: bin\insightedge-pyspark.cmd [options]
+
+rem Figure out which Python to use.
+if "x%PYSPARK_DRIVER_PYTHON%"=="x" (
+  set PYSPARK_DRIVER_PYTHON=python
+  if not [%PYSPARK_PYTHON%] == [] set PYSPARK_DRIVER_PYTHON=%PYSPARK_PYTHON%
+)
+
+set PYTHONPATH=%SPARK_HOME%\python;%PYTHONPATH%
+set PYTHONPATH=%SPARK_HOME%\python\lib\py4j-0.10.4-src.zip;%PYTHONPATH%
+
+rem Load the InsighEdge version of shell.py script:
+set OLD_PYTHONSTARTUP=%PYTHONSTARTUP%
+set PYTHONSTARTUP=%XAP_HOME%\insightedge\bin\shell-init.py
+
+call "%SPARK_HOME%\bin\spark-submit" pyspark-shell-main --name "PySparkShell" --driver-class-path=%INSIGHTEDGE_CORE_CP% --conf spark.executor.extraClassPath=%INSIGHTEDGE_CORE_CP% %*
