f63b423e3ee3d143d5b5e54315b5ab966ce0163b
Tamir Schwarz <tamirs2005@gmail.com>
2017-09-14 09:58:00 +0300

    XAP-13306 fix init of InsightEdgeConfig in shell-init

--- insightedge-packager/src/main/resources/bin/shell-init.scala
+++ insightedge-packager/src/main/resources/bin/shell-init.scala
@@ -6,7 +6,7 @@ import org.insightedge.spark.implicits.all._
 
 System.setProperty("net.jini.discovery.LookupLocatorDiscovery.level", "OFF")
 
-val ieConfig = InsightEdgeConfig(sys.env.get("INSIGHTEDGE_SPACE_NAME"))
+val ieConfig = InsightEdgeConfig(sys.env("INSIGHTEDGE_SPACE_NAME"))
 val sparkMaster = spark.conf.get("spark.master", "local[*]")
 spark.stopInsightEdgeContext()
 val spark = SparkSession.builder.master(sparkMaster).appName("my init shell").insightEdgeConfig(ieConfig).getOrCreate()
