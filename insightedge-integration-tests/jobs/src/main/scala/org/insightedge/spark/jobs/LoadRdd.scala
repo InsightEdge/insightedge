a6db9f666ccb94d69e502469e5996678b1da1b6d
alonshoham <alon.shoham@gmail.com>
2017-09-27 13:08:51 +0300

    LoadRddFailover failing i9e test

--- insightedge-integration-tests/jobs/src/main/scala/org/insightedge/spark/jobs/LoadRdd.scala
+++ insightedge-integration-tests/jobs/src/main/scala/org/insightedge/spark/jobs/LoadRdd.scala
@@ -34,7 +34,7 @@ object LoadRdd {
     val Array(master, space) = settings
     val config = InsightEdgeConfig(space)
     val sc = new SparkContext(new SparkConf().setAppName("example-load-rdd").setMaster(master).setInsightEdgeConfig(config))
-
+    Thread.sleep(30000)
     val rdd = sc.gridRdd[Product]()
     println(s"Products RDD count: ${rdd.count()}")
     sc.stopInsightEdgeContext()
