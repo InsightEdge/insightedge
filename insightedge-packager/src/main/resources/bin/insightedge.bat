6a31c6dcb883e00b07cb0e4c8697aa0a326f9d8c
Yohana Khoury <yohana@yohanakhoury.com>
2018-03-07 19:19:09 +0200

    GS-13438 sync scripts CP and Java options

--- insightedge-packager/src/main/resources/bin/insightedge.bat
+++ insightedge-packager/src/main/resources/bin/insightedge.bat
@@ -2,5 +2,5 @@
 SETLOCAL EnableDelayedExpansion
 call %~dp0..\insightedge\conf\insightedge-env.cmd
 
-set INSIGHTEDGE_CLI_CP="%XAP_HOME%\tools\cli\*";%INSIGHTEDGE_CLASSPATH%
-java %XAP_OPTIONS% -cp %INSIGHTEDGE_CLI_CP% org.insightedge.cli.commands.I9EMainCommand %*
+set INSIGHTEDGE_CLI_CP="%XAP_HOME%\tools\cli\*";%INSIGHTEDGE_CLASSPATH%;%SIGAR_JARS%
+%JAVACMD% %JAVA_OPTIONS% %XAP_OPTIONS% %XAP_CLI_OPTIONS% -cp %INSIGHTEDGE_CLI_CP% org.insightedge.cli.commands.I9EMainCommand %*
