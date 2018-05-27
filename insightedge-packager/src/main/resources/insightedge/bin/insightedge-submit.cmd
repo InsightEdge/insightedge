eb079327b710ae1ba31cf481a214bba272c72040
alonshoham <alon.shoham@gmail.com>
2018-05-27 13:39:14 +0300

    GS-13557 remove insightedge-submit scripts

--- insightedge-packager/src/main/resources/insightedge/bin/insightedge-submit.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-call %~dp0..\conf\insightedge-env.cmd
-call %SPARK_HOME%\bin\spark-submit --driver-class-path=%INSIGHTEDGE_CLASSPATH% --conf spark.executor.extraClassPath=%INSIGHTEDGE_CLASSPATH% %*
