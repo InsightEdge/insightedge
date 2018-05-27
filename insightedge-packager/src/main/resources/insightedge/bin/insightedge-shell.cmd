1313297d51c114676cf062d74cc1779c570a18b1
alonshoham <alon.shoham@gmail.com>
2018-05-27 13:54:16 +0300

    GS-13557 update shell scripts
    (cherry picked from commit c07357f)

--- insightedge-packager/src/main/resources/insightedge/bin/insightedge-shell.cmd
+++ insightedge-packager/src/main/resources/insightedge/bin/insightedge-shell.cmd
@@ -3,4 +3,4 @@ call %~dp0..\conf\insightedge-env.cmd
 
 rem SPARK_SUBMIT_OPTS adds -Dspark.driver.extraClassPath + -Dspark.executor.extraClassPath which the spark-class2 do not like
 set SPARK_SUBMIT_OPTS=
-"%SPARK_HOME%\bin\spark-shell2.cmd" --driver-class-path=%INSIGHTEDGE_CLASSPATH% -i %~dp0\shell-init.scala  %*
+"%SPARK_HOME%\bin\spark-shell2.cmd" -i %~dp0\shell-init.scala  %*
