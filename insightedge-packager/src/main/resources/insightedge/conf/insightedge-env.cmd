e902653df7bed35c48ec3a4c3fc67378f269082a
alonshoham <alon.shoham@gmail.com>
2018-05-30 07:46:44 +0300

    GS-13557 cleanup
    (cherry picked from commit 8a3cab2)

--- insightedge-packager/src/main/resources/insightedge/conf/insightedge-env.cmd
+++ insightedge-packager/src/main/resources/insightedge/conf/insightedge-env.cmd
@@ -10,10 +10,10 @@ rem Source XAP environment:
 call %~dp0..\..\bin\setenv.bat
 
 rem Set InsightEdge defaults:
-set INSIGHTEDGE_CLASSPATH="%XAP_HOME%\insightedge\lib\*;%XAP_HOME%\lib\required\*;%XAP_HOME%\lib\optional\spatial\*"
+set INSIGHTEDGE_CLASSPATH=%XAP_HOME%\insightedge\lib\*;%XAP_HOME%\lib\required\*;%XAP_HOME%\lib\optional\spatial\*
 if defined INSIGHTEDGE_CLASSPATH_EXT set INSIGHTEDGE_CLASSPATH=%INSIGHTEDGE_CLASSPATH_EXT%;%INSIGHTEDGE_CLASSPATH%
 
-if not defined HADOOP_HOME set HADOOP_HOME="%XAP_HOME%\insightedge\tools\winutils"
+if not defined HADOOP_HOME set HADOOP_HOME=%XAP_HOME%\insightedge\tools\winutils
 if not defined SPARK_HOME set SPARK_HOME=%XAP_HOME%\insightedge\spark
 
 rem InsightEdge dependencies to Spark
