19c08238f22e993e477a60b8c50a01090e4d5c29
alonshoham <alon.shoham@gmail.com>
2018-06-04 08:55:00 +0300

    GS-13557 new insightedge-submit scripts

--- insightedge-packager/src/main/resources/insightedge/bin/insightedge-submit.cmd
+++ insightedge-packager/src/main/resources/insightedge/bin/insightedge-submit.cmd
@@ -1,26 +1,26 @@
 @echo off
 call %~dp0..\conf\insightedge-env.cmd
 
+rem loop over arguments to find if in cluster deploy-mode
 :loop
-set key=%1
 
-if [%key%] == [] goto EOF
-if [%key%] == [--deploy-mode] (
-	set value=%2
+if [%1] == [] goto END_LOOP
+if [%1] == [--deploy-mode] (
 
-    if [%value%] == [cluster] (
-
+    if [%2] == [cluster] (
+        rem In cluster mode, local env variables override remote machine env variables (spark JIRA SPARK-24456)
+        rem To work around this behavior:
         rem 1. stop spark submit to source spark-env
         set SPARK_ENV_LOADED=1
 
-        rem 2. empty i9e local classpaths env variable
+        rem 2. Unset i9e local classpaths env variable
         set SPARK_DIST_CLASSPATH=
 
-		goto EOF
+		goto END_LOOP
     )
 )
 shift
 goto loop
 
-:EOF
+:END_LOOP
 call %SPARK_HOME%\bin\spark-submit %*
