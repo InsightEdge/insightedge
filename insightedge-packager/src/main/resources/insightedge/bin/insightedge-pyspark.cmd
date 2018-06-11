79678d1130fec48f36bdc81268d4907acb230dc9
ayeletmorris <39298591+ayeletmorris@users.noreply.github.com>
2018-06-11 15:45:04 +0300

    change where we define python and pyspark env parameters

--- insightedge-packager/src/main/resources/insightedge/bin/insightedge-pyspark.cmd
+++ insightedge-packager/src/main/resources/insightedge/bin/insightedge-pyspark.cmd
@@ -12,13 +12,6 @@ call %SPARK_HOME%\bin\load-spark-env.cmd
 set _SPARK_CMD_USAGE=Usage: bin\insightedge-pyspark.cmd [options]
 
 rem Figure out which Python to use.
-if "x%PYSPARK_DRIVER_PYTHON%"=="x" (
-  set PYSPARK_DRIVER_PYTHON=python
-  if not [%PYSPARK_PYTHON%] == [] set PYSPARK_DRIVER_PYTHON=%PYSPARK_PYTHON%
-)
-
-set PYTHONPATH=%SPARK_HOME%\python;%PYTHONPATH%
-set PYTHONPATH=%SPARK_HOME%\python\lib\py4j-0.10.6-src.zip;%PYTHONPATH%
 
 rem Load the InsighEdge version of shell.py script:
 set OLD_PYTHONSTARTUP=%PYTHONSTARTUP%
