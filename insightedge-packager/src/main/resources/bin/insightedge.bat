e16c1e87d1a5a623bb95408fa48d0e3a8c127a7a
alonshoham <alon.shoham@gmail.com>
2019-02-03 08:12:05 +0200

    GS-13754 fixed insightedge.bat script

--- insightedge-packager/src/main/resources/bin/insightedge.bat
+++ insightedge-packager/src/main/resources/bin/insightedge.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 call %~dp0..\insightedge\conf\insightedge-env.cmd
-FOR /F "tokens=*" %%i IN ('"%JAVACMD% -cp "%XAP_HOME%\lib\required\*"" org.insightedge.cli.commands.I9ECommandFactory cli-insightedge') DO set GS_COMMAND=%%i %*
+FOR /F "tokens=*" %%i IN ('"%JAVACMD% -cp "%XAP_HOME%\lib\required\*";"%XAP_HOME%\tools\cli\*"" org.insightedge.cli.commands.I9ECommandFactory cli-insightedge') DO set GS_COMMAND=%%i %*
 if "!GS_COMMAND:~0,6!"=="Error:" (
   echo %GS_COMMAND%
 ) else (
