06d3caa768432bb4d0ef69e0f624eb0cfda100ac
alonshoham <alon.shoham@gmail.com>
2019-03-04 14:49:03 +0200

    GS-13662 Added analytics Xtreme dependencies to InsightEdge classpath

--- insightedge-packager/src/main/resources/insightedge/conf/insightedge-env.cmd
+++ insightedge-packager/src/main/resources/insightedge/conf/insightedge-env.cmd
@@ -6,12 +6,15 @@ rem * If you need to override the defaults, please modify $XAP_HOME\bin\setenv-o
 rem * the XAP_SETTINGS_FILE environment variable to your custom script.                                       *
 rem * For more information see http://docs.gigaspaces.com/xap/12.3/dev-java/common-environment-variables.html *
 rem ***********************************************************************************************************
-rem Source XAP environment:
+rem Source XAP environment;
 call %~dp0..\..\bin\setenv.bat
 
-rem Set InsightEdge defaults:
+rem Set InsightEdge defaults;
 set INSIGHTEDGE_CLASSPATH=%XAP_HOME%\insightedge\lib\*;%XAP_HOME%\insightedge\lib\jdbc\*;%XAP_HOME%\insightedge\lib\analytics-xtreme\*;%XAP_HOME%\lib\required\*;%XAP_HOME%\lib\optional\spatial\*
-if defined INSIGHTEDGE_CLASSPATH_EXT set INSIGHTEDGE_CLASSPATH=%INSIGHTEDGE_CLASSPATH_EXT%;%INSIGHTEDGE_CLASSPATH%
+set ANALYTICS_XTREME_CLASSPATH=%XAP_HOME%\insightedge\lib\analytics-xtreme\*;%XAP_HOME%\lib\platform\service-grid\*
+
+set INSIGHTEDGE_CLASSPATH=%INSIGHTEDGE_CLASSPATH%;%ANALYTICS_XTREME_CLASSPATH%
+if defined INSIGHTEDGE_CLASSPATH_EXT set INSIGHTEDGE_CLASSPATH=%INSIGHTEDGE_CLASSPATH%;%INSIGHTEDGE_CLASSPATH_EXT%
 
 if not defined HADOOP_HOME set HADOOP_HOME=%XAP_HOME%\insightedge\tools\winutils
 if not defined SPARK_HOME set SPARK_HOME=%XAP_HOME%\insightedge\spark
@@ -19,9 +22,11 @@ if not defined SPARK_HOME set SPARK_HOME=%XAP_HOME%\insightedge\spark
 rem InsightEdge dependencies to Spark
 if not defined SPARK_DIST_CLASSPATH set SPARK_DIST_CLASSPATH=%INSIGHTEDGE_CLASSPATH%
 
+
 rem Zeppelin
 if not defined ZEPPELIN_PORT set ZEPPELIN_PORT=9090
-if not defined ZEPPELIN_INTP_CLASSPATH_OVERRIDES set ZEPPELIN_INTP_CLASSPATH_OVERRIDES=%INSIGHTEDGE_CLASSPATH%
+rem Spark jars are added to interpreter classpath because of Analytics Xtreme
+if not defined ZEPPELIN_INTP_CLASSPATH_OVERRIDES set ZEPPELIN_INTP_CLASSPATH_OVERRIDES=%INSIGHTEDGE_CLASSPATH%;%SPARK_HOME%\jars\*
 if not defined ZEPPELIN_LOG_DIR set ZEPPELIN_LOG_DIR=%XAP_HOME%\logs
 
 if not defined INSIGHTEDGE_SPACE_NAME set INSIGHTEDGE_SPACE_NAME=demo
