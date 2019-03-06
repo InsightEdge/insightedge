fd540cd94b01b2874cfcd508134d5ce00e4a6426
alonshoham <alon.shoham@gmail.com>
2019-03-06 07:08:21 +0200

    GS-13705 upgrade spark version to 2.4.0

--- insightedge-packager/src/main/resources/insightedge/bin/shell-init.scala
+++ insightedge-packager/src/main/resources/insightedge/bin/shell-init.scala
@@ -1,14 +1,7 @@
-import org.apache.spark.{SparkConf, SparkContext}
 import org.insightedge.spark.context.InsightEdgeConfig
-import org.insightedge.spark.implicits.all._
-import org.apache.spark.sql.SparkSession
 import org.insightedge.spark.implicits.all._
 
 System.setProperty("net.jini.discovery.LookupLocatorDiscovery.level", "OFF")
 
 val ieConfig = InsightEdgeConfig(sys.env("INSIGHTEDGE_SPACE_NAME"))
-val sparkMaster = spark.conf.get("spark.master", "local[*]")
-spark.stopInsightEdgeContext()
-val spark = SparkSession.builder.master(sparkMaster).appName("my init shell").insightEdgeConfig(ieConfig).getOrCreate()
-val sc = spark.sparkContext
-import spark.implicits._
+sc.initializeInsightEdgeContext(ieConfig)
