e3e5da7f7dab7c86bd4e7c896b9e10ebe3add871
yaelna <nahon.yael@gmail.com>
2019-04-04 19:24:19 +0300

    GS-13814 don't copy i9e-core pom in build script

--- insightedge-packager/src/main/scala/org/insightedge/spark/packager/Launcher.scala
+++ insightedge-packager/src/main/scala/org/insightedge/spark/packager/Launcher.scala
@@ -65,7 +65,6 @@ object Launcher {
       }
       run("Adding poms of integration libs") {
         copy(s"$project/pom.xml", s"$insightEdgeHome/tools/maven/poms/insightedge-package/pom.xml")
-        copy(s"$project/insightedge-core/pom.xml", s"$insightEdgeHome/tools/maven/poms/insightedge-core/pom.xml")
       }
       run("Adding examples") {
         val examplesModule = s"$examplesTarget/.."
