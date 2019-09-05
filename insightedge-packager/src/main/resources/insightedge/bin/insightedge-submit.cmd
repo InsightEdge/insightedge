e1eec6b74f383c5032ac38dbd3c32cf850e00773
mavigdor <moran@gigaspaces.com>
2019-09-05 16:11:09 +0300

    GS-13884 Replace environment variable prefix with GS instead of XAP in scripts

--- insightedge-packager/src/main/resources/insightedge/bin/insightedge-submit.cmd
+++ insightedge-packager/src/main/resources/insightedge/bin/insightedge-submit.cmd
@@ -2,7 +2,7 @@
 call %~dp0..\conf\insightedge-env.cmd
 
 rem Because of SPARK-21642, the driver url is composed from hostname instead of ip. This is a workaround - setting SPARK_LOCAL_HOSTNAME to the submitting machine IP
-if not defined SPARK_LOCAL_HOSTNAME set SPARK_LOCAL_HOSTNAME=%XAP_NIC_ADDRESS%
+if not defined SPARK_LOCAL_HOSTNAME set SPARK_LOCAL_HOSTNAME=%GS_NIC_ADDRESS%
 
 rem loop over arguments to find if in cluster deploy-mode
 :loop
--- insightedge-packager/src/main/resources/insightedge/conf/insightedge-env.cmd
+++ insightedge-packager/src/main/resources/insightedge/conf/insightedge-env.cmd
@@ -2,20 +2,20 @@
 rem ***********************************************************************************************************
 rem * This script is used to initialize common environment to GigaSpaces InsightEdge platform.                *
 rem * It is highly recommended NOT TO MODIFY THIS SCRIPT, to simplify future upgrades.                        *
-rem * If you need to override the defaults, please modify $XAP_HOME\bin\setenv-overrides.bat or set           *
-rem * the XAP_SETTINGS_FILE environment variable to your custom script.                                       *
+rem * If you need to override the defaults, please modify $GS_HOME\bin\setenv-overrides.bat or set           *
+rem * the GS_SETTINGS_FILE environment variable to your custom script.                                       *
 rem * For more information see https://docs.gigaspaces.com/14.5/started/common-environment-variables.html     *
 rem ***********************************************************************************************************
 rem Source XAP environment;
 call %~dp0..\..\bin\setenv.bat
 
 rem Set InsightEdge defaults;
-set INSIGHTEDGE_CLASSPATH=%XAP_HOME%\insightedge\lib\*;%XAP_HOME%\insightedge\lib\jdbc\*;%XAP_HOME%\insightedge\lib\analytics-xtreme\*;%XAP_HOME%\lib\required\*;%XAP_HOME%\lib\optional\spatial\*
+set INSIGHTEDGE_CLASSPATH=%GS_HOME%\insightedge\lib\*;%GS_HOME%\insightedge\lib\jdbc\*;%GS_HOME%\insightedge\lib\analytics-xtreme\*;%GS_HOME%\lib\required\*;%GS_HOME%\lib\optional\spatial\*
 
 if defined INSIGHTEDGE_CLASSPATH_EXT set INSIGHTEDGE_CLASSPATH=%INSIGHTEDGE_CLASSPATH%;%INSIGHTEDGE_CLASSPATH_EXT%
 
-if not defined HADOOP_HOME set HADOOP_HOME=%XAP_HOME%\insightedge\tools\winutils
-if not defined SPARK_HOME set SPARK_HOME=%XAP_HOME%\insightedge\spark
+if not defined HADOOP_HOME set HADOOP_HOME=%GS_HOME%\insightedge\tools\winutils
+if not defined SPARK_HOME set SPARK_HOME=%GS_HOME%\insightedge\spark
 
 rem InsightEdge dependencies to Spark
 if not defined SPARK_DIST_CLASSPATH set SPARK_DIST_CLASSPATH=%INSIGHTEDGE_CLASSPATH%
@@ -25,7 +25,7 @@ rem Zeppelin
 if not defined ZEPPELIN_PORT set ZEPPELIN_PORT=9090
 rem Spark jars are added to interpreter classpath because of Analytics Xtreme
 if not defined ZEPPELIN_INTP_CLASSPATH_OVERRIDES set ZEPPELIN_INTP_CLASSPATH_OVERRIDES=%INSIGHTEDGE_CLASSPATH%;%SPARK_HOME%\jars\*
-if not defined ZEPPELIN_LOG_DIR set ZEPPELIN_LOG_DIR=%XAP_HOME%\logs
+if not defined ZEPPELIN_LOG_DIR set ZEPPELIN_LOG_DIR=%GS_HOME%\logs
 
 if not defined INSIGHTEDGE_SPACE_NAME set INSIGHTEDGE_SPACE_NAME=demo
 
